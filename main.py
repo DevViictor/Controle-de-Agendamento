ce01b393e1a4459d8e26323f13a337f13b037868
DevVictor <victorritossantos@gmail.com>
2025-11-20 11:45:53 -0300

    Gerenciador de Fibra

---
+++ main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+gcp_info = st.secrets["gcp"]
+planilha_chave = st.secrets["planilha"]["chave"]
+
+# Criar credenciais
+creds = Credentials.from_service_account_info(
+    dict(gcp_info),
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+)
+
+client = gspread.authorize(creds)
+
+pagina = client.open_by_key(planilha_chave).worksheet("dados")
+
+st.set_page_config(page_title= "Acompanhamento das Fibras" ,page_icon = "🌐" , layout= "wide")
+
+lojas = [" ","LOJA IGUATEMI | BA" , "LOJA IGUATEMI || BA"]
+
+consultores = ["" , "ANA" , "ANDERSON" , "AMANDA" , "DAVID" , "DEBORA" , "LENE" , "LORENA" , "RODRIGO"]
+
+with st.form("Cadastro"):
+    st.header("🌐 Agendamento de Fibra")
+    Loja = st.selectbox("Loja:",lojas)
+    nome = st.selectbox("Nome do Consultor: ",consultores,key="nome")
+    ordem = st.text_input("Número da ordem: ",key="ordem")
+    data = st.date_input("Data do agendamento: " ,key="data")
+    hora = st.time_input("Hora de instalação: ",key="hora")
+    email = st.text_input("Email do consultor: ",key="email")
+    menssagem = st.text_input("Menssagem de lembrete para o consultor: ",key="lembrete")
+
+    enviar = st.form_submit_button("Cadastrar")
+
+if enviar:
+
+    pagina.append_row([
+        Loja,
+        str(nome),
+        ordem,
+        str(data),
+        str(hora),
+        email,
+        menssagem
+    ])
+    st.success("Agendamento realizado com sucesso!")
